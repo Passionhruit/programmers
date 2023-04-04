298336b85730251f7f14e9990aa585567c40dd74
Mia Lee <92542456+Passionhruit@users.noreply.github.com>
2023-04-04 22:04:00 +1000

    [level 0] Title: 합성수 찾기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,11 +1,23 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
 function solution(n) {
     var sum = 0;
-    var array = []
-    for(i = 1; i <= n; i++) {
-        for(j = 2; j < i; j++) {
-            if(i%j == 0) {
-                array.push(i)
+    for (i = 4; i <= n; i ++) {
+        for (j = 2; j < i; j ++) {
+            if(i % j == 0) {
+                sum = sum + 1;
+                break
             }
         }
-    } return [... new Set(array)].length;
+    } return sum;
 }
