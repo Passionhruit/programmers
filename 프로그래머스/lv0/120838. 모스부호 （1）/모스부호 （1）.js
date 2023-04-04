249a656ff3cec240e7e99b022f75a890559fdfd8
Mia Lee <92542456+Passionhruit@users.noreply.github.com>
2023-04-04 15:23:38 +1000

    [level 0] Title: 모스부호 (1), Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,14 @@
+function solution(letter) {
+    let answer = ""
+    const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    letter = letter.split(" ");
+    for (i = 0; i < letter.length; i++) {
+        answer = answer + morse[letter[i]]
+    } return answer
+}
