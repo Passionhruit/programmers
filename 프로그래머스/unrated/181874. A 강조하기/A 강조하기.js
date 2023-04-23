9fe03e7aac89fe0a4c9405e0d03ebf155a9d521a
Mia Lee <92542456+Passionhruit@users.noreply.github.com>
2023-04-23 19:18:00 +1000

    [unrated] Title: A 강조하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
@@ -1,10 +1,3 @@
 function solution(myString) {
-    var array = myString.split("");
-    for(i = 0; i < array.length; i++) {
-        if(array[i] == "a" || array[i] == "A") {
-            array[i] = "A"
-        }else{
-            array[i] = array[i].toLowerCase();
-        }
-    } return array.join("")
+    return [...myString].map(value => ["a","A"].includes(value) ? "A" : value.toLowerCase()).join("")
 }
