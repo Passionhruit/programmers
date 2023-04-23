7126edf39ab9369e27aa85bfb1f7266458f257ca
Mia Lee <92542456+Passionhruit@users.noreply.github.com>
2023-04-23 17:05:52 +1000

    [unrated] Title: 이어 붙인 수, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
@@ -2,7 +2,7 @@ function solution(num_list) {
     var odd = '';
     var even = '';
     num_list.forEach((a) => {
-        a % 2 == 1 ? odd = odd + a : even = even + a
+        a % 2 == 1 ? odd += a : even += a
     })
     return odd*1 + even*1;
 }
