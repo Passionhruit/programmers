7305797db8e853b304c851b5502b9af9d2d6bd70
Mia Lee <92542456+Passionhruit@users.noreply.github.com>
2023-04-30 19:21:03 +1000

    [unrated] Title: 수 조작하기 2, Time: 10.68 ms, Memory: 41.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,14 @@
+function solution(numLog) {
+    var answer = [];
+    for(i = 1; i < numLog.length; i++) {
+        if(numLog[i]-numLog[i-1] === 1) {
+            answer.push("w")
+        }else if(numLog[i]-numLog[i-1] === -1) {
+            answer.push("s")
+        }else if(numLog[i]-numLog[i-1] === 10) {
+            answer.push("d")
+        }else {
+            answer.push("a")
+        }
+    } return answer.join("")
+}
