6f1bf6b670f5d274b87327882cb3cb404b9c3c8e
Mia Lee <92542456+Passionhruit@users.noreply.github.com>
2023-05-04 19:12:37 +1000

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,3 +1,9 @@
+/*
+function solution(str, over, s) {
+   return str.replace(str.slice(s,over.length+s),over)
+}
+*/
+
 function solution(str, over, s) {
    return str.slice(0,s)+over+str.slice(s+over.length,str.length)
 }
