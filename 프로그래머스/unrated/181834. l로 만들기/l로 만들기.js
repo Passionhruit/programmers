4565c2ed03b1cd7543094574c645adce7bcdd3af
Mia Lee <92542456+Passionhruit@users.noreply.github.com>
2023-05-06 20:15:54 +1000

    [unrated] Title: l로 만들기, Time: 6.64 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.js
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.js
@@ -1,5 +1,4 @@
 function solution(myString) {
-    var before = ["a","b","c","d","e","f","g","h","i","j","k"] 
     var array = myString.split("");
-    return array.map(v => before.includes(v) ? v = "l" : v).join("")
+    return array.map(v => v < "l" ? v = "l" : v).join("")
 }
