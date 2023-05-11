af704a08a78774313f7730a8cf41d62044b63740
Mia Lee <92542456+Passionhruit@users.noreply.github.com>
2023-05-11 20:10:03 +1000

    [unrated] Title: 코드 처리하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
@@ -1,6 +1,7 @@
 function solution(code) {
     var mode = 0;
-    var answer = []
+    var answer = [];
+    
     for(i = 0; i < code.length; i++) {
         if(mode == 0 && code[i] == "1") {
             mode = 1;
@@ -11,5 +12,5 @@ function solution(code) {
         }else if(mode == 1 && code[i] !== "1") {
             i % 2 == 1 ? answer.push(code[i]) : answer;
         }
-    } return answer.length !== 0 ? answer.join("") : "EMPTY"
+    } return answer.length == 0 ? "EMPTY" : answer.join("");
 }
