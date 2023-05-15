8b9027cfa5d2627f252e7721836824b92ebbe287
Mia Lee <92542456+Passionhruit@users.noreply.github.com>
2023-05-15 11:08:00 +0800

    [unrated] Title: x 사이의 개수, Time: 4.63 ms, Memory: 38 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.js
@@ -0,0 +1,14 @@
+function solution(myString) {
+    var answer = [];
+    var sum = 0;
+    for(i = 0; i < myString.length; i++) {
+        if(myString[i] !== "x") {
+            sum = sum + 1
+        }else {
+            answer.push(sum)
+            sum = 0;
+        }
+    } 
+    answer.push(sum)
+    return answer
+}
