f68814e28392ddf9c0df27a84e1f8dd1706c2b3e
Mia Lee <92542456+Passionhruit@users.noreply.github.com>
2023-05-20 23:01:33 +0900

    [unrated] Title: 글자 지우기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
@@ -1,10 +1,3 @@
 function solution(my_string, indices) {
-    var arr = my_string.split("")
-    for(i = 0; i < arr.length; i++) {
-        for(j = 0; j < indices.length; j++) {
-            if(i == indices[j]) {
-                arr[i] = ""
-            }
-        }
-    } return arr.join("")
+    return [...my_string].filter((v,i) => !indices.includes(i)).join("")
 }
