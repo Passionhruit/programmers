a6af12fe4a009cd6afa2361b9843122984c8d9d3
Mia Lee <92542456+Passionhruit@users.noreply.github.com>
2023-05-22 19:21:21 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,6 +1,7 @@
-function solution(n) {
-    var last = n[n.length-1]
-    var secLast = n[n.length-2]
-    last > secLast ? n.push(last-secLast) : n.push(last*2)
-    return n
+function solution(num_list) {
+    var last = num_list[num_list.length-1];
+    var secLast = num_list[num_list.length-2];
+    
+    last > secLast ? num_list.push(last-secLast) : num_list.push(last*2)
+    return num_list
 }
