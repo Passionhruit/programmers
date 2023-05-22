a8663b5c4db079c3a4df6e8caba4dbbec7edf4c1
Mia Lee <92542456+Passionhruit@users.noreply.github.com>
2023-05-22 18:33:08 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 3.78 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,11 +1,11 @@
 function solution(arr, queries) {
   const result = [];
 
-  for (i = 0; i < queries.length; i++) {
-    var [s, e, k] = queries[i];
+  for (let i = 0; i < queries.length; i++) {
+    const [s, e, k] = queries[i];
     let min = -1;
 
-    for (j = s; j <= e; j++) {
+    for (let j = s; j <= e; j++) {
       if (arr[j] > k && (min === -1 || arr[j] < min)) {
         min = arr[j];
       }
