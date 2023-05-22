50ba05e9db9a38f8382c109d89500889cc7dc62e
Mia Lee <92542456+Passionhruit@users.noreply.github.com>
2023-05-22 16:48:46 +0900

    [unrated] Title: 수 조작하기 1, Time: 3.07 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,5 +1,10 @@
 function solution(n, control) {
-    const word = { "w": +1, "s": -1, "d": +10, "a": -10, }
+    const word = {
+        "w": +1,
+        "s": -1,
+        "d": +10,
+        "a": -10,
+    }
     for(i = 0; i < control.length; i++) {
         n = n + word[control[i]]
     } return n
