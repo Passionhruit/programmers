2b3b15e603c79cb197fb48fae8a371097663a78c
Mia Lee <92542456+Passionhruit@users.noreply.github.com>
2023-05-25 17:29:09 +0900

    [unrated] Title: 문자열 여러 번 뒤집기, Time: 10.53 ms, Memory: 35 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -0,0 +1,15 @@
+function solution(my_string, queries) {
+    for(i = 0; i < queries.length; i++) {
+        var [s,e] = queries[i]
+        var part = my_string.slice(s,e+1)
+        var reversed = [...part].reverse().join("")
+        my_string = my_string.slice(0,s) + reversed + my_string.slice(e+1,my_string.length)
+    }
+    return my_string;
+}
+
+
+
+
+
+
