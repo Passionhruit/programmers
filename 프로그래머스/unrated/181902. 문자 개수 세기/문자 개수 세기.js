64a98e1483015f8b16edcc33c23ed7abd9b1f07e
Mia Lee <92542456+Passionhruit@users.noreply.github.com>
2023-06-01 09:35:09 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -2,24 +2,12 @@ function solution(my_string) {
     var sum = 0
     var answer = []
     var low = "abcdefghijklmnopqrstuvwxyz"
-    var up = low.toUpperCase()
-    for(i = 0; i < up.length; i++) {
-        if(my_string.includes(up[i])) {
-            for(j = 0; j < my_string.length; j++) {
-                if(up[i] == my_string[j])
-                    sum = sum + 1;
-            }
-            answer.push(sum)
-            sum = 0;
-        }else{
-            answer.push(0)
-        }
-    }
+    var total = low.toUpperCase() + low
     
-    for(i = 0; i < low.length; i++) {
-        if(my_string.includes(low[i])) {
+    for(i = 0; i < total.length; i++) {
+        if(my_string.includes(total[i])) {
             for(j = 0; j < my_string.length; j++) {
-                if(low[i] == my_string[j])
+                if(total[i] == my_string[j])
                     sum = sum + 1;
             }
             answer.push(sum)
