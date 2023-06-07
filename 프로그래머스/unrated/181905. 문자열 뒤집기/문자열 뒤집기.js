5ff6602fc65c302d201ee2d6b1d0b63760e7ea00
Mia Lee <92542456+Passionhruit@users.noreply.github.com>
2023-06-07 19:30:05 +0900

    [unrated] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,5 +1,3 @@
 function solution(my_string, s, e) {
-    var array = my_string.split("")
-    var reversed = [...array].splice(s,e-s+1).reverse().join("")
-    return array.slice(0,s).join("")+reversed+array.slice(e+1,array.length).join("")
+    return my_string.slice(0,s) + my_string.slice(s,e+1).split("").reverse().join("") + my_string.slice(e+1)
 }
