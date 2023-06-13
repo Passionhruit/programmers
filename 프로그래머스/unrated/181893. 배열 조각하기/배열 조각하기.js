ccaafdd79a7e03da443aa557e30433ea83e51d67
Mia Lee <92542456+Passionhruit@users.noreply.github.com>
2023-06-13 19:46:36 +0900

    [unrated] Title: 배열 조각하기, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.js
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.js
@@ -1,9 +1,9 @@
 function solution(arr, query) {
     for(i = 0; i < query.length; i++) {
         if(i % 2 == 0) {
-            arr = arr.splice(0, query[i]+1)
+            arr.splice(query[i]+1)
         }else {
-            arr = arr.splice(query[i])
+            arr.splice(0, query[i])
         }
     } return arr
 }
