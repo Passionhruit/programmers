bd904d41c06076c4de4d58caaccdfe9685d0d69d
Mia Lee <92542456+Passionhruit@users.noreply.github.com>
2023-07-04 19:40:13 +0900

    [unrated] Title: 배열 비교하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.js
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.js
@@ -1,16 +1,27 @@
 function solution(arr1, arr2) {
-    var sum1 = 0;
-    var sum2 = 0;
+    var len1 = arr1.length;
+    var len2 = arr2.length;
+    var sum1 = 0
+    var sum2 = 0
     
-    if(arr1.length > arr2.length) {
+    for(i = 0; i < len1; i++) {
+        sum1 += arr1[i]
+    }
+    
+    for(i = 0; i < len2; i++) {
+        sum2 += arr2[i]
+    }
+        
+    if(len1 > len2) {
         return 1
-    }else if(arr1.length < arr2.length) {
+    }else if (len2 > len1){
+        return -1
+    }else if(sum1 > sum2) {
+        return 1
+    }else if(sum2 > sum1) {
         return -1
     }else {
-        for(i = 0; i < arr1.length; i++) {
-            sum1 += arr1[i]
-            sum2 += arr2[i]
-        }
-        return sum1 > sum2 ? 1 : sum1 == sum2 ? 0 : -1
+        return 0
     }
+    
 }
