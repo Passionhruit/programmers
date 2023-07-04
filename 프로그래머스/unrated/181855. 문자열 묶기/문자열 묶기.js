46968dcb51b6b77df6ee281d9b72b826aa650255
Mia Lee <92542456+Passionhruit@users.noreply.github.com>
2023-07-04 19:55:18 +0900

    [unrated] Title: 문자열 묶기, Time: 5.22 ms, Memory: 56.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.js
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.js
@@ -1,21 +1,5 @@
 function solution(strArr) {
-    let nums = strArr.map((v)=> v = v.length)
-    let count = {}
-    for(let num of nums) {
-        if(num in count) {
-            count[num] += 1
-        }else{
-            count[num] = 1
-        }    
-    }
-    
-    let max_num = 0
-    let answer = [];
-    
-    for(let num in count) {
-        if(count[num] > max_num) {
-            max_num = count[num]
-            answer = count[num]
-        }
-    } return answer
+    let ans = Array(31).fill(0);
+    for (let s of strArr) ans[s.length]++
+    return Math.max(...ans)
 }
