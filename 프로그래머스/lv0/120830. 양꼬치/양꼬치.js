3e22291818fbe7183ee0e8865bf7eccbc7144a6b
Mia <92542456+Passionhruit@users.noreply.github.com>
2023-08-01 09:40:29 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -1,10 +1,6 @@
 function solution(n, k) {
-    let lamb = 12000
-    let drink = 2000
+    let lamb = 12000;
+    let drink = 2000;
     
-    if(n>=10) {
-        return 12000*n+drink*k-parseInt(n/10)*2000
-    }else{
-       return 12000*n+drink*k 
-    }
+    return n > 10 ? lamb*n+drink*k-parseInt(n/10)*2000 : lamb*n+drink*k
 }
