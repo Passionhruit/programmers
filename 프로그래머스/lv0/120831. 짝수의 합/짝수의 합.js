106d0a78c4d15a5815da76e777318cdaa7507f5a
Mia <92542456+Passionhruit@users.noreply.github.com>
2023-08-01 09:46:31 +0900

    [level 0] Title: 짝수의 합, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -1,6 +1,7 @@
 function solution(n) {
     let sum = 0;
-    for(i = 2; i <= n; i++) {
-        i % 2 == 0 ? sum = sum + i : null
-    } return sum
+    for(i=2; i<=n; i++) {
+        i%2 == 0 ? sum = sum + i : null
+    }
+    return sum
 }
