68ebf68e2aee29d7e125c8ad892010537dd7e6aa
Mia <92542456+Passionhruit@users.noreply.github.com>
2023-08-03 09:13:46 +0900

    [level 0] Title: 개미 군단, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,11 +1,11 @@
 function solution(hp) {
-    for (i = 0; i <= hp; i++) {
-        for(j = 0; j <= hp; j ++) {
-            for (k = 0; k <= hp; k++) {
-                if(hp == 5 * k + 3 * j + i) {
-                    return k + j + i
+    for(i=0; i<=hp; i++) {
+        for(j=0; j<=hp; j++) {
+            for(k=0; k<=hp; k++) {
+                if(i+3*j+5*k === hp) {
+                    return i+j+k
                 }
             }
-        }   
+        }
     }
 }
