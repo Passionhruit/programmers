38a649b33d37e06b3f9cfe2491bcc65544748fa2
Mia <92542456+Passionhruit@users.noreply.github.com>
2023-08-04 09:04:23 +0900

    [level 0] Title: 공 던지기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -1,5 +1,5 @@
 function solution(numbers, k) {
-    if ((1 + 2 * (k-1)) % numbers.length == 0) {
+    if ((1 + 2 * (k-1)) % numbers.length === 0) {
         return numbers[numbers.length-1]
     } else {
         return (1 + 2 * (k-1)) % numbers.length 
