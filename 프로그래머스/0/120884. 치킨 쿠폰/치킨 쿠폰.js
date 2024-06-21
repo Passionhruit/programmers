5861d59b11679e35190b90b892beaa331521559c
Mia <92542456+Passionhruit@users.noreply.github.com>
2024-06-21 16:05:24 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,11 +1,3 @@
 function solution(chicken) {
-    let coupons = chicken;
-    let totalServices = 0;
-    
-    while(coupons >= 10) {
-        let service = Math.floor(coupons / 10);
-        totalServices += service;
-        coupons = coupons % 10 + service
-    }
-    return totalServices;
+return parseInt(chicken*0.111111)
 }
