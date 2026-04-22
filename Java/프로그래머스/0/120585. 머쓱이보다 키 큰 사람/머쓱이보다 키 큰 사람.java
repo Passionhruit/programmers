fac5ca6eb851948286313265d36c0ddc2080d1e6
Mia <92542456+Passionhruit@users.noreply.github.com>
2026-04-22 15:40:33 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] array, int height) {
+        int count = 0;
+        for (int num : array) {
+            if (num > height) {
+                count ++;
+            }
+        }
+        return count;
+    }
+}
