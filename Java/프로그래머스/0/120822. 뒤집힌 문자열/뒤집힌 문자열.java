6d73cbfe7989eca834041ee07de0de4b246d5f81
Mia <92542456+Passionhruit@users.noreply.github.com>
2026-04-23 18:07:33 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.05 ms, Memory: 76.5 MB -BaekjoonHub

--- Java/프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
+++ Java/프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -1,10 +1,5 @@
 class Solution {
-    public String solution(String my_string) {
-        String[] arr = my_string.split("");
-        String[] newArr = new String[arr.length];
-        for (int i = 0; i <= arr.length - 1; i++) {
-            newArr[i] = arr[arr.length - 1 -i];
-        }
-        return String.join("", newArr);
+    public Object solution(String my_string) {
+        return new StringBuilder(my_string).reverse();
     }
 }
