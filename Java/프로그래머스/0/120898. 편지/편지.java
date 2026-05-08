3d4483bf6484dec4f6d2b021bbbd9c3d489dc46e
Mia <92542456+Passionhruit@users.noreply.github.com>
2026-05-08 14:49:03 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

--- Java/프로그래머스/0/120898. 편지/편지.java
+++ Java/프로그래머스/0/120898. 편지/편지.java
@@ -1,9 +1,5 @@
 class Solution {
     public int solution(String message) {
-        int count = 0;
-        for(int i = 0; i < message.split("").length; i++) {
-            count = count + 1;
-        }
-        return count * 2;
+        return message.length() * 2;
     }
 }
